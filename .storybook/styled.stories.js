76fdf23bcec1b08c6db18e949596eabb0bea78f7
Vadim Dalecky <streamich@gmail.com>
2018-03-13 21:03:05 +0000

    test: add styled() stories

---
+++ .storybook/styled.stories.js
@@ -0,0 +1,29 @@
+import {createElement as h} from 'react';
+import {storiesOf} from '@storybook/react';
+const {action} = require('@storybook/addon-actions');
+const {linkTo} = require('@storybook/addon-links');
+const {create} = require('../lib');
+
+const renderer = create(h);
+const {styled} = renderer;
+
+const RedBorder = styled('div', {
+  border: '1px solid red'
+}, (props) => {
+    if (props.primary) {
+        return {
+            color: 'blue'
+        };
+    }
+});
+
+storiesOf('styled()', module)
+    .add('Default', () =>
+        h(RedBorder, null, 'Hello world')
+    )
+    .add('Custom CSS', () =>
+        h(RedBorder, {primary: true}, 'Hello world')
+    )
+    .add('Inline styles', () =>
+        h(RedBorder, {style: {color: 'red'}, primary: true}, 'Hello world')
+    )
