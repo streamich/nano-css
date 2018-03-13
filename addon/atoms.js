edc5d5928b4a42920839fce078c5164867215ef2
Vadim Dalecky <streamich@gmail.com>
2018-03-13 22:55:32 +0000

    feat: add atom addon

---
+++ addon/atoms.js
@@ -0,0 +1,57 @@
+'use strict';
+
+var atoms = {
+    d: 'display',
+
+    mar: 'margin',
+    mart: 'margin-top',
+    marr: 'margin-right',
+    marb: 'margin-bottom',
+    marl: 'margin-left',
+    pad: 'padding',
+    padt: 'padding-top',
+    padr: 'padding-right',
+    padb: 'padding-bottom',
+    padl: 'padding-left',
+
+    bd: 'border',
+    bdt: 'border-top',
+    bdr: 'border-right',
+    bdb: 'border-bottom',
+    bdl: 'border-left',
+    bdrad: 'border-radius',
+
+    col: 'color',
+    op: 'opacity',
+    bg: 'background',
+    bgc: 'background-color',
+
+    fz: 'font-size',
+    fs: 'font-style',
+    fw: 'font-weight',
+    ff: 'font-family',
+
+    lh: 'line-height',
+    bxz: 'box-sizing',
+    cur: 'cursor',
+    ov: 'overflow',
+    pos: 'position',
+    ls: 'list-style',
+    ta: 'text-align',
+    td: 'text-decoration',
+    fl: 'float',
+    w: 'width',
+    h: 'height',
+    trs: 'transition',
+    out: 'outline',
+    vis: 'visibility',
+    ww: 'word-wrap',
+    con: 'content',
+};
+
+exports.addon = function (renderer) {
+    var originalKey = renderer.key;
+    renderer.key = function (key, value) {
+        return originalKey(atoms[key] || key, value);
+    };
+};
