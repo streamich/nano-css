50fa6464b8ab4ffa81594617ce2f2401519dec46
Vadim Dalecky <streamich@gmail.com>
2018-03-14 15:25:55 +0000

    test: add drule() stories

---
+++ .storybook/drule.stories.js
@@ -0,0 +1,26 @@
+import {createElement as h} from 'react';
+import {storiesOf} from '@storybook/react';
+const {action} = require('@storybook/addon-actions');
+const {linkTo} = require('@storybook/addon-links');
+const {create} = require('../index');
+const {addon: addonRule} = require('../addon/rule');
+const {addon: addonDrule} = require('../addon/drule');
+const {addon: addonCache} = require('../addon/cache');
+
+const renderer = create();
+addonCache(renderer);
+addonRule(renderer);
+addonDrule(renderer);
+const {drule} = renderer;
+
+const className1 = drule({
+  border: '1px solid red'
+}, 'RedBorder');
+
+storiesOf('drule()', module)
+  .add('Default', () =>
+    h('div', {className: '' + className1}, 'Hello world')
+  )
+  .add('Dynamic styles', () =>
+    h('div', {className: className1({fontWeight: 'bold'})}, 'Hello world')
+  )
--- addon/drule.js
+++ addon/drule.js
@@ -2,13 +2,17 @@
 
 exports.addon = function (renderer) {
     if (process.env.NODE_ENV !== 'production') {
-        require('./__dev__/warnOnMissingDependencies')('drule', renderer, ['rule']);
+        require('./__dev__/warnOnMissingDependencies')('drule', renderer, ['rule', 'cache']);
     }
 
     renderer.drule = function (styles, block) {
         var className = renderer.rule(styles, block);
 
         var closure = function (dynamicStyles) {
+            if (!dynamicStyles) {
+                return className;
+            }
+
             var dynamicClassName = renderer.cache(dynamicStyles);
 
             return className + dynamicClassName;
