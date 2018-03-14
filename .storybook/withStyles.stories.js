26d8d9a8619a250b11bc4adca8860b9acc038d59
Vadim Dalecky <streamich@gmail.com>
2018-03-14 00:30:38 +0000

    test: add withStyles() story

---
+++ .storybook/withStyles.stories.js
@@ -0,0 +1,32 @@
+import {createElement as h} from 'react';
+import {storiesOf} from '@storybook/react';
+const {action} = require('@storybook/addon-actions');
+const {linkTo} = require('@storybook/addon-links');
+const {create} = require('../lib');
+
+const renderer = create(h);
+require('../addon/sheet').addon(renderer);
+require('../addon/withStyles').addon(renderer);
+const {withStyles} = renderer;
+
+const styles = {
+    tomato: {
+        border: '1px solid tomato',
+    },
+    yellow: {
+        border: '1px solid yellow',
+    },
+};
+
+const Example1 = withStyles(styles, function Example1 (props) {
+    var styles = props.styles;
+    return h('div', null,
+        h('div', {className: styles.tomato}, 'Red'),
+        h('div', {className: styles.yellow}, 'Yellow'),
+    )
+});
+
+storiesOf('Addons/withStyles()', module)
+    .add('Default', () =>
+        h(Example1)
+    )
--- addon/withStyles.js
+++ addon/withStyles.js
@@ -4,7 +4,7 @@ exports.addon = function (renderer) {
     renderer.withStyles = function (map, fn, block) {
         block = block || fn.displayName || fn.name;
 
-        var styles = this.sheet(map, block);
+        var styles = renderer.sheet(map, block);
         var Component = function (props) {
             if (process.env.NODE_ENV !== 'production') {
                 return fn(Object.assign({}, props, {
