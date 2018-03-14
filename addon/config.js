eb3459f3041a610a7d494119d223f0c44f0ed75d
Vadim Dalecky <streamich@gmail.com>
2018-03-14 13:39:02 +0000

    refactor: rename extend() to assign()

--- addon/config.js
+++ addon/config.js
@@ -12,14 +12,14 @@ var hash = function (str) {
 
 exports.addon = function (renderer, config) {
     config = config || {};
-    var extend = config.extend || Object.assign;
+    var assign = config.assign || Object.assign;
 
-    extend(renderer, {
+    assign(renderer, {
         raw: '',
         cns: {},
         pfx: '_',
         client: typeof window === 'object',
-        extend: extend,
+        assign: assign,
         stringify: JSON.stringify,
         decl: function (key, value) {
             key = key.replace(KEBAB_REGEX, '-$&').toLowerCase();
