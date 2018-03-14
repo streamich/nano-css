51b09b3d51559d3dbd3c673a1da8b343872020b0
Vadim Dalecky <streamich@gmail.com>
2018-03-14 00:57:54 +0000

    feat: add basic static class decorator

---
+++ addon/decorator.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var cloneElement = require('react').cloneElement;
+
+exports.addon = function (renderer) {
+    var transformStatic = function (fn, styles, block) {
+        block = block || fn.displayName || fn.name;
+
+        var render_ = fn.prototype.render;
+        var className = '';
+
+        fn.prototype.render = function() {
+            var element = render_.call(this);
+
+            if (element) {
+                if (!className) {
+                    className = renderer.rule(styles, block);
+                }
+
+                if (process.env.NODE_ENV === 'production') {
+                    element.props.className = (element.props.className || '') + className;
+                } else {
+                    element = cloneElement(element, {
+                        className: (element.props.className || '') + className,
+                    });
+                }
+            }
+
+            return element;
+        };
+    };
+
+    var decorator = function (a, b) {
+        return function (Klass) {
+            transformStatic(Klass, a, b);
+        };
+    };
+
+    return decorator;
+};
