049202a40c167a6173f862aeef9a34967bcfc869
Vadim Dalecky <streamich@gmail.com>
2018-03-15 02:49:16 +0000

    test: add hyperstyle() stories

---
+++ .storybook/hyperstyle.stories.js
@@ -0,0 +1,29 @@
+import {createElement} from 'react';
+import {storiesOf} from '@storybook/react';
+const {action} = require('@storybook/addon-actions');
+const {linkTo} = require('@storybook/addon-links');
+const {create} = require('../index');
+const {addon: addonSheet} = require('../addon/sheet');
+const {addon: addonHyperstyle} = require('../addon/hyperstyle');
+
+const renderer = create({h: createElement});
+addonSheet(renderer);
+addonHyperstyle(renderer);
+const {hyperstyle} = renderer;
+
+const h = hyperstyle({
+    tomato: {
+        border: '1px solid tomato',
+    },
+    yellow: {
+        border: '1px solid yellow',
+    },
+}, 'hello');
+
+storiesOf('hyperstyle()', module)
+    .add('Default', () =>
+        h('div', null,
+            h('div', {styleName: 'tomato'}, 'Red'),
+            h('div', {styleName: 'yellow'}, 'Yellow'),
+        )
+    )
