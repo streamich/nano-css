040dddb9a7d6f0a75e09c03a37fb7f780bc1a7e1
Vadim Dalecky <streamich@gmail.com>
2018-03-18 12:37:47 +0000

    chore: fix prefixer story

--- .storybook/prefixer.stories.js
+++ .storybook/prefixer.stories.js
@@ -4,7 +4,7 @@ const {action} = require('@storybook/addon-actions');
 const {linkTo} = require('@storybook/addon-links');
 const {create} = require('../index');
 const {addon: addonRule} = require('../addon/rule');
-const {addon: addonPrefixer} = require('../addon/inline-style-prefixer');
+const {addon: addonPrefixer} = require('../addon/prefixer');
 
 const renderer = create();
 addonRule(renderer);
@@ -12,13 +12,14 @@ addonPrefixer(renderer);
 const {rule} = renderer;
 
 const className1 = rule({
+    flex: 1,
     display: 'flex',
     alignItems: 'center',
     boxShadow: '0 0 5px red',
     'text-shadow': '2px 2px #ff0000',
 });
 
-storiesOf('Addons/inline-style-prefixer', module)
+storiesOf('Addons/prefixer', module)
     .add('Default', () =>
         h('div', {className: className1}, 'Hello world')
     )
