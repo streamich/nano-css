ae542c018489f842736b462120c2a2940e91bfc9
Vadim Dalecky <streamich@gmail.com>
2018-03-24 16:41:43 +0000

    feat: add react preset

--- docs/Presets.md
+++ docs/Presets.md
@@ -25,3 +25,4 @@ Below is a list of available presets.
 
 - `sheet` &mdash; installs [`sheet()`](./sheet.md) addon, as well as `stable`, `nesting`, `atoms`, and `keyframes` addons
 - `vdom` &mdash; similar to `sheet` preset, but also installs [`jsx()`](./jsx.md) addon, you need to provide `h` function in configuration
+- `react` &mdash; similar to `vdom` preset, but specifies hyperscript function `h` automatically and also installs `snake`, `style`, `styled`, and `decorator` addons
--- preset/react.js
+++ preset/react.js
@@ -1,24 +1,23 @@
 'use strict';
 
+var h = require('react').createElement;
 var create = require('../index').create;
 var addonCache = require('../addon/cache').addon;
 var addonStable = require('../addon/stable').addon;
 var addonNesting = require('../addon/nesting').addon;
 var addonAtoms = require('../addon/atoms').addon;
+var addonSnake = require('../addon/snake').addon;
 var addonKeyframes = require('../addon/keyframes').addon;
 var addonRule = require('../addon/rule').addon;
 var addonSheet = require('../addon/sheet').addon;
 var addonJsx = require('../addon/jsx').addon;
+var addonStyle = require('../addon/style').addon;
+var addonStyled = require('../addon/styled').addon;
+var addonDecorator = require('../addon/decorator').addon;
 
 exports.preset = function (config) {
-    if (process.env.NODE_ENV !== 'production') {
-        if (!config || !(config instanceof Object) || !config.h) {
-            console.error(
-                'For "vdom" nano-css preset you have to provide virtual DOM ' +
-                'hyperscript function h. Such as: preset({h: require("react").createElement})'
-            );
-        }
-    }
+    config = config || {};
+    config.h = config.h || h;
 
     var nano = create(config);
 
@@ -26,10 +25,14 @@ exports.preset = function (config) {
     addonStable(nano);
     addonNesting(nano);
     addonAtoms(nano);
+    addonSnake(nano);
     addonKeyframes(nano);
     addonRule(nano);
     addonSheet(nano);
     addonJsx(nano);
+    addonStyle(nano);
+    addonStyled(nano);
+    addonDecorator(nano);
 
     return nano;
 };
