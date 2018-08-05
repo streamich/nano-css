5d6d21bcf554c161a087276e4db23c778adfcb83
streamich <streamich@gmail.com>
2018-08-05 21:03:08 +0200

    fix: 🐛 add !important only if it is not already there

--- addon/important.js
+++ addon/important.js
@@ -1,9 +1,36 @@
 'use strict';
 
+function hasImportant (rawDecl) {
+    var parts = rawDecl.split(' ');
+
+    for (var i = 0; i < parts.length; i++) {
+        var part = parts[i].trim();
+
+        if (part === '!important') return true;
+    }
+
+    return false;
+}
+
 exports.addon = function (renderer) {
     var decl = renderer.decl;
 
     renderer.decl = function (prop, value) {
-        return decl(prop, value).replace(/;/g, ' !important;');
+        var rawDecl = decl(prop, value);
+        var decls = rawDecl.split(';');
+        var css = '';
+
+        for (var i = 0; i < decls.length; i++) {
+            rawDecl = decls[i];
+
+            // Don't add "!important" if it is already added.
+            if (!hasImportant(rawDecl)) {
+                css += rawDecl + ' !important;';
+            } else {
+                css += rawDecl + ';';
+            }
+        }
+
+        return css;
     };
 };
