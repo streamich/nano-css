e5e83c4989f1f224b08f27e83d19f1a44885f694
Nathan <nathankerr@gmail.com>
2018-09-18 00:05:11 -0700

    feat: better prefixing
    * Properly kebab ms prefix
    
    Also added test
    
    * Reworked prefixer to be smarter
    
    Instead of blindly splitting on ';', we can instead return an object from .prefixer() and then use that to build the string.
    
    I think it's cleaner this way.
    
    Closes #206

--- addon/__tests__/prefixer.test.js
+++ addon/__tests__/prefixer.test.js
@@ -39,4 +39,19 @@ describe('prefixer', function () {
 		});
 
 	});
+
+	it('doesn\'t kebab values', function() {
+		var nano = createNano();
+		var decl = {
+			'background-image': 'url("data:image/svg+xml;utf8,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' xmlns:xlink=\'http://www.w3.org/1999/xlink\' width=\'48\' height=\'36\' viewBox=\'0 0 48 36\' fill=\'rgb(28,28,28)\'%3E%3Crect x=\'16\' y=\'12\' width=\'16\' height=\'2\' /%3E%3Crect x=\'16\' y=\'17\' width=\'16\' height=\'2\' /%3E%3Crect x=\'16\' y=\'22\' width=\'16\' height=\'2\' /%3E%3C/svg>")'
+		};
+
+		nano.putRaw = jest.fn();
+
+		nano.put('.one', decl);
+
+		var result = nano.putRaw.mock.calls[0][0].replace(/ +(?= )/g,'');
+		var expected = '.one{background-image:url("data:image/svg+xml;utf8,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' xmlns:xlink=\'http://www.w3.org/1999/xlink\' width=\'48\' height=\'36\' viewBox=\'0 0 48 36\' fill=\'rgb(28,28,28)\'%3E%3Crect x=\'16\' y=\'12\' width=\'16\' height=\'2\' /%3E%3Crect x=\'16\' y=\'17\' width=\'16\' height=\'2\' /%3E%3Crect x=\'16\' y=\'22\' width=\'16\' height=\'2\' /%3E%3C/svg>");}';
+		expect(result).toEqual(expected);
+	});
 });
