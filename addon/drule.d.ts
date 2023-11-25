5282c01c187579747c1ce576d9b7ec135098d909
Vadims Daleckis <streamich@gmail.com>
2023-11-26 00:05:28 +0100

    fix: make dynamic CSS object optional

--- addon/drule.d.ts
+++ addon/drule.d.ts
@@ -1,8 +1,8 @@
-import {CssLikeObject, TDynamicCss} from '../types/common';
+import {CssLikeObject} from '../types/common';
 import {NanoRenderer} from '../types/nano';
 
 export interface DruleAddon {
-    drule: (css: CssLikeObject, block?: string) => TDynamicCss;
+    drule: (css: CssLikeObject, block?: string) => (css?: CssLikeObject) => string;
 }
 
 export function addon(nano: NanoRenderer);
